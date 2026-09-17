520d2b995f00a0dfba32e8135c373ce9368fd2b2
slshan0597 <slshan0597@gmail.com>
2026-09-17 15:52:32 +0900

    Lv 14. 같은 월드의 참여자에게 채팅 전송

--- src/main/java/com/gameexpert/chat/service/LocalChatSender.java
+++ src/main/java/com/gameexpert/chat/service/LocalChatSender.java
@@ -11,5 +11,6 @@ public class LocalChatSender {
 
     public void send(Long worldId, Object message) {
         // TODO Lv 14: 같은 월드의 참여자에게 메시지를 전송합니다.
+        broadcaster.broadcast(worldId, message);
     }
 }
